7545beb14a4758e39f5f3caca4d94a5723525220
Gonzalo <g.castillo.dlf@gmail.com>
2022-11-28 15:23:32 -0300

    Crear migracion de procedimientos

--- database/migrations/2022_11_28_182317_create__procedimientos.php
+++ database/migrations/2022_11_28_182317_create__procedimientos.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEquipos extends Migration
+class CreateProcedimientos extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateEquipos extends Migration
      */
     public function up()
     {
-        Schema::create('Equipos', function (Blueprint $table) {
+        Schema::create('Procedimientos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateEquipos extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Equipos');
+        Schema::dropIfExists('Procedimientos');
     }
 }
