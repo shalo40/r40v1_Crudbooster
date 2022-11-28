29a21a5d823e8a2735de2bd00ccadb047214c2ee
Gonzalo <g.castillo.dlf@gmail.com>
2022-11-27 21:33:48 -0300

    nullables clientes

--- database/migrations/2022_11_27_235534_create_clientes.php
+++ database/migrations/2022_11_27_235534_create_clientes.php
@@ -16,13 +16,13 @@ class CreateClientes extends Migration
         Schema::create('clientes', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->String('Nombre_Cliente');
-            $table->String('Apellido1_Cliente');
-            $table->String('Apellido2_Cliente');
-            $table->String('Direccion_Cliente');
-            $table->String('Correo_Cliente');
-            $table->String('Telefono_Cliente');
-            $table->String('Rut_Cliente');
+            $table->String('Nombre_Cliente')->nullable();
+            $table->String('Apellido1_Cliente')->nullable();
+            $table->String('Apellido2_Cliente')->nullable();
+            $table->String('Direccion_Cliente')->nullable();
+            $table->String('Correo_Cliente')->nullable();
+            $table->String('Telefono_Cliente')->nullable();
+            $table->String('Rut_Cliente')->nullable();
 
 
         });
